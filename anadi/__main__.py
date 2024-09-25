c1581997bd3d6adc239248a2a7f7cd383a83bd18
mklno <manoprasanth.mathavan@gmail.com>
2024-09-25 11:09:36 +0530

    execute query via cli

--- anadi/__main__.py
+++ anadi/__main__.py
@@ -3,11 +3,12 @@ import argparse
 
 from anadi.app import AnadiApp
 from anadi.constants import *
-
+from anadi.csvdb import CSVDB
+from anadi.models.confs import SettingsApp, SettingsDB
 
 parser = argparse.ArgumentParser(prog="AnadiCSV", description="Handle CVS file using SQL language")
 parser.add_argument("-d", "--dir", type=str, help="CSV file dir", required=True)
-
+parser.add_argument("-q", "--query", type=str, help="SQL query to execute", required=False)
 
 
 def run():
@@ -16,7 +17,13 @@ def run():
     try:
         app = AnadiApp()
         app.init(args.dir, os.path.abspath(os.path.expanduser(ANADI_CONF_FILE)))
-        app.run()
+        if args.query:
+            csvdb = CSVDB()
+            csvdb.load(args.dir,SettingsDB())
+            result = csvdb.raw_sql(args.query)
+            print(result)
+        else:
+            app.run()
     except Exception as ex:
         print(ex)
         with open("error.log", 'w') as errfile:

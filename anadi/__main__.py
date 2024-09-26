681bba52ddc388204aa21edefc17c1fac7e83aa8
mklno <manoprasanth.mathavan@gmail.com>
2024-09-26 19:26:14 +0530

    either dir or file and query should be passed

--- anadi/__main__.py
+++ anadi/__main__.py
@@ -7,24 +7,29 @@ from anadi.csvdb import CSVDB
 from anadi.models.confs import SettingsApp, SettingsDB
 
 parser = argparse.ArgumentParser(prog="AnadiCSV", description="Handle CVS file using SQL language")
-parser.add_argument("-d", "--dir", type=str, help="CSV file dir", required=True)
+parser.add_argument("-d", "--dir", type=str, help="CSV file dir", required=False)
 parser.add_argument("-f", "--file", type=str, help="CSV file name", required=False)
 parser.add_argument("-q", "--query", type=str, help="SQL query to execute", required=False)
 
 
 def run():
     args = parser.parse_args()
-
+    
+    if not args.dir and not(args.file and args.query) or args.dir and (args.file and args.query):
+        raise ValueError("Error: Either --dir must be provided, or both --file and --query must be provided.")
+    if args.dir and args.file:
+        raise ValueError("Error: Cannot user both --dir  and --file together")        
     try:
         app = AnadiApp()
-        app.init(args.dir, os.path.abspath(os.path.expanduser(ANADI_CONF_FILE)))
-        if args.query:
+        if args.dir:
+            app.init(args.dir, os.path.abspath(os.path.expanduser(ANADI_CONF_FILE)))
+            app.run()
+        elif args.file and args.query:
             csvdb = CSVDB()
             csvdb.load(args.file,SettingsDB())
             result = csvdb.raw_sql(args.query)
             print(result)
-        else:
-            app.run()
+        
     except Exception as ex:
         print(ex)
         with open("error.log", 'w') as errfile:

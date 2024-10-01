8929b3ad49dcfaf12fea567b028801ac092eb9d6
Alessandro Pischedda <alessandro.pischedda@gmail.com>
2024-10-01 21:18:17 +0200

    code format with black

--- anadi/__main__.py
+++ anadi/__main__.py
@@ -5,19 +5,30 @@ from anadi.constants import *
 from anadi.csvdb import CSVDB
 from anadi.models.confs import SettingsApp, SettingsDB
 
-parser = argparse.ArgumentParser(prog="AnadiCSV", description="Handle CVS file using SQL language")
+parser = argparse.ArgumentParser(
+    prog="AnadiCSV", description="Handle CVS file using SQL language"
+)
 parser.add_argument("-d", "--dir", type=str, help="CSV file dir", required=False)
 parser.add_argument("-f", "--file", type=str, help="CSV file name", required=False)
-parser.add_argument("-q", "--query", type=str, help="SQL query to execute", required=False)
+parser.add_argument(
+    "-q", "--query", type=str, help="SQL query to execute", required=False
+)
 
 
 def run():
     args = parser.parse_args()
-    
-    if not args.dir and not(args.file and args.query) or args.dir and (args.file and args.query):
-        raise ValueError("Error: Either --dir must be provided, or both --file and --query must be provided.")
+
+    if (
+        not args.dir
+        and not (args.file and args.query)
+        or args.dir
+        and (args.file and args.query)
+    ):
+        raise ValueError(
+            "Error: Either --dir must be provided, or both --file and --query must be provided."
+        )
     if args.dir and args.file:
-        raise ValueError("Error: Cannot user both --dir  and --file together")        
+        raise ValueError("Error: Cannot user both --dir  and --file together")
     try:
         app = AnadiApp()
         if args.dir:
@@ -25,13 +36,13 @@ def run():
             app.run()
         elif args.file and args.query:
             csvdb = CSVDB()
-            csvdb.load(args.file,SettingsDB())
+            csvdb.load(args.file, SettingsDB())
             result = csvdb.raw_sql(args.query)
             print(result)
-        
+
     except Exception as ex:
         print(ex)
-        with open("error.log", 'w') as errfile:
+        with open("error.log", "w") as errfile:
             errfile.write(f"{ex}")
 
 
